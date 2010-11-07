c68b2b18681f9e311cd2a62e3aa6c22e5d1c3c0c
reacocard <reacocard@cogito.(none)>
2010-11-07 10:54:00 -0600

    Modify xinit scripts to handle multiple distros better.

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-eval `dbus-launch --sh-syntax --exit-with-session`
 xmodmap -e "remove Lock = Caps_Lock"
 xmodmap -e "clear mod3"
 xmodmap -e "add Mod3 = Caps_Lock"
@@ -8,6 +7,17 @@ xsetroot -cursor_name left_ptr
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 12 &
 wicd-client &
 blueman-applet &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+
+
+# TODO: This is ugly, use a loop.
+POLKIT_BIN=/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+if [ -x $POLKIT_BIN ]; then
+    $POLKIT_BIN &
+else
+    POLKIT_BIN=/usr/libexec/polkit-gnome-authentication-agent-1
+    if [ -x $POLKIT_BIN ]; then
+        $POLKIT_BIN &
+    fi
+fi
 
 exec xmonad
