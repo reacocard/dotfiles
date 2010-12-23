c68e4c2b2fb70f97d84cb6fd397e257687c5a5ee
Ayla Ounce <reacocard@gmail.com>
2010-12-22 18:46:22 -0600

    tweaks to startup scripts

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -12,7 +12,7 @@ xmodmap -e "add Mod3 = Caps_Lock"
 xsetroot -cursor_name left_ptr
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 12 &
 wicd-client &
-blueman-applet &
+#blueman-applet &
 
 
 # TODO: This is ugly, use a loop.
