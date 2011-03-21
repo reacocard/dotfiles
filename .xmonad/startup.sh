d9b45e0a97a61ad4ebd9123e14fef8c27aad5746
Ayla Ounce <reacocard@gmail.com>
2011-03-21 14:23:44 -0500

    startup: update xmodmap invocations to work with xkeyboard-config 2.1

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -7,8 +7,14 @@ fi
 
 
 xmodmap -e "remove Lock = Caps_Lock"
-xmodmap -e "clear mod3"
-xmodmap -e "add Mod3 = Caps_Lock"
+xmodmap -e "keysym Caps_Lock = ISO_Level5_Shift"
+xmodmap -e "add mod3 = ISO_Level5_Shift"
+
+# these don't work as of xkeyboard-config 2.1
+#xmodmap -e "remove Lock = Caps_Lock"
+#xmodmap -e "clear mod3"
+#xmodmap -e "add Mod3 = Caps_Lock"
+
 xsetroot -cursor_name left_ptr
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 12 &
 wicd-client &
