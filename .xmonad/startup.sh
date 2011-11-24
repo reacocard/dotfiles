cea49983f4f93dce58678b28ece35cf8228e7c05
Ayla Ounce <reacocard@gmail.com>
2011-11-24 12:08:10 -0800

    Add ssh-agent to startup

--- .xmonad/startup.sh
+++ .xmonad/startup.sh
@@ -5,6 +5,12 @@ if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRE
     eval `$dbuslaunch --sh-syntax --exit-with-session`
 fi
 
+sshagent="`which ssh-agent 2>/dev/null`"
+if [ -n "$sshagent" ] && [ -x "$sshagent" ] && [ -z "$SSH_AUTH_SOCK" ]; then
+    eval `$sshagent`
+fi
+
+
 xsetroot -cursor_name left_ptr
 trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 12 &
 
