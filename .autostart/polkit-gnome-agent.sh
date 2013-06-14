ef2e4dd20e788c6bb60272869c10eeacf8357cd8
Ayla Ounce <reacocard@gmail.com>
2013-06-13 21:46:38 -0700

    Update polkit autostart for new path.

--- .autostart/polkit-gnome-agent.sh
+++ .autostart/polkit-gnome-agent.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
-/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
+
+PATHS='/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1'
+
+for path in $PATHS; do
+    if [ -x "$path" ]; then
+        $path &
+        break
+    fi
+done
