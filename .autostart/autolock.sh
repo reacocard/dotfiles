4ecc38f01a8afd3212ce3d2cdcbcaf9f03ad9c62
Ayla Ounce <reacocard@gmail.com>
2014-02-13 22:59:05 -0800

    Update screenlock config

--- .autostart/autolock.sh
+++ .autostart/autolock.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-exec xautolock
+# Disable X default screensaver
+xset s off
+
+# 11 minute dpms blank - lock is at 10.25 minutes.
+xset dpms 0 0 660
+
+exec xautolock -locker "xlock -lockdelay 15" -nowlocker "xlock"
