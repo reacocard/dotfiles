325e288f37ca6fc649d36d598c86ec831097dc00
Ayla Ounce <reacocard@gmail.com>
2014-12-18 15:36:13 -0600

    Add browser launch keybindings.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -187,6 +187,8 @@ volmuteCmd = "~/bin/pulsevolume mute"
 lockCmd = "xautolock -locknow"
 sleepCmd = "~/bin/lockandsleep.sh"
 touchpadCmd = "~/bin/toggle_touchpad.sh"
+browserCmd = "google-chrome-stable"
+privateBrowserCmd = "google-chrome-stable --incognito"
 
 myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((0, xK_XF86VolumeUp),    spawn volupCmd) 
@@ -208,29 +210,31 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
     ++
-    [ ((modm,               xK_Return), spawn $ XMonad.terminal conf)
-    , ((modm,               xK_p     ), spawn "dmenu_run")
-    , ((modm .|. shiftMask, xK_p     ), spawn "gmrun")
-    , ((modm .|. shiftMask, xK_c     ), kill)
-    , ((modm,               xK_space ), sendMessage NextLayout)
-    , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
-    , ((modm,               xK_n     ), refresh)
-    , ((modm,               xK_Tab   ), windows W.focusDown)
-    , ((modm,               xK_j     ), windows W.focusDown)
-    , ((modm,               xK_k     ), windows W.focusUp)
-    , ((modm,               xK_m     ), windows W.focusMaster)
-    , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
-    , ((modm .|. shiftMask, xK_j     ), windows W.swapDown)
-    , ((modm .|. shiftMask, xK_k     ), windows W.swapUp)
-    , ((modm,               xK_h     ), sendMessage Shrink)
-    , ((modm,               xK_l     ), sendMessage Expand)
-    , ((modm .|. shiftMask, xK_h     ), sendMessage MirrorExpand)
-    , ((modm .|. shiftMask, xK_l     ), sendMessage MirrorShrink)
-    , ((modm,               xK_t     ), withFocused $ windows . W.sink)
-    , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
-    , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    [ ((modm,               xK_Return    ), spawn $ XMonad.terminal conf)
+    , ((modm,               xK_apostrophe), spawn browserCmd)
+    , ((modm .|. shiftMask, xK_apostrophe), spawn privateBrowserCmd)
+    , ((modm,               xK_p         ), spawn "dmenu_run")
+    , ((modm .|. shiftMask, xK_p         ), spawn "gmrun")
+    , ((modm .|. shiftMask, xK_c         ), kill)
+    , ((modm,               xK_space     ), sendMessage NextLayout)
+    , ((modm .|. shiftMask, xK_space     ), setLayout $ XMonad.layoutHook conf)
+    , ((modm,               xK_n         ), refresh)
+    , ((modm,               xK_Tab       ), windows W.focusDown)
+    , ((modm,               xK_j         ), windows W.focusDown)
+    , ((modm,               xK_k         ), windows W.focusUp)
+    , ((modm,               xK_m         ), windows W.focusMaster)
+    , ((modm .|. shiftMask, xK_Return    ), windows W.swapMaster)
+    , ((modm .|. shiftMask, xK_j         ), windows W.swapDown)
+    , ((modm .|. shiftMask, xK_k         ), windows W.swapUp)
+    , ((modm,               xK_h         ), sendMessage Shrink)
+    , ((modm,               xK_l         ), sendMessage Expand)
+    , ((modm .|. shiftMask, xK_h         ), sendMessage MirrorExpand)
+    , ((modm .|. shiftMask, xK_l         ), sendMessage MirrorShrink)
+    , ((modm,               xK_t         ), withFocused $ windows . W.sink)
+    , ((modm              , xK_comma     ), sendMessage (IncMasterN 1))
+    , ((modm              , xK_period    ), sendMessage (IncMasterN (-1)))
+    , ((modm .|. shiftMask, xK_q         ), io (exitWith ExitSuccess))
+    , ((modm              , xK_q         ), spawn "xmonad --recompile; xmonad --restart")
     ]
 
 myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
