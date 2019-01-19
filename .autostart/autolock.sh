a763c9e90bc3a77f3983620eaad05242681d4ec6
Ayla Ounce <reacocard@gmail.com>
2019-01-18 17:53:57 -0800

    [lock] add pre-lock screen dim

--- .autostart/autolock.sh
+++ .autostart/autolock.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
 LOCK_TIME="${REACOCARD_SCREEN_LOCK_TIME:-1800}"
+CYCLE_TIME="${REACOCARD_SCREEN_LOCK_CYCLE_TIME:-15}"  # aka notifier timeout
 LOCK_CMD="${REACOCARD_SCREEN_LOCK_COMMAND:-xsecurelock}"
 DIMMER="/usr/lib/x86_64-linux-gnu/xsecurelock/dimmer"
 
-xset s ${LOCK_TIME}
+xset s ${LOCK_TIME} ${CYCLE_TIME}
 
 # X11 blank after lock.
-XSECURELOCK_BLANK_TIMEOUT=5
+export XSECURELOCK_BLANK_TIMEOUT=5
+export XSECURELOCK_WAIT_TIME_MS=${CYCLE_TIME}000
 
 exec xss-lock --notifier=${DIMMER} --transfer-sleep-lock -- ${LOCK_CMD}
--- bin/lockandsleep.sh
+++ bin/lockandsleep.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 xset s activate
+xset dpms force off
 #xautolock -locknow
 
 systemctl suspend || upower_suspend.sh
