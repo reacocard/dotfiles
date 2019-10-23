26002069b6fb974d6aa880c216ee69591bece464
Ayla Ounce <reacocard@gmail.com>
2019-10-22 17:10:57 -0700

    [lock] update dimmer path; set dpms timeout

--- .autostart/autolock.sh
+++ .autostart/autolock.sh
@@ -3,9 +3,10 @@
 LOCK_TIME="${REACOCARD_SCREEN_LOCK_TIME:-1800}"
 CYCLE_TIME="${REACOCARD_SCREEN_LOCK_CYCLE_TIME:-15}"  # aka notifier timeout
 LOCK_CMD="${REACOCARD_SCREEN_LOCK_COMMAND:-xsecurelock}"
-DIMMER="/usr/lib/x86_64-linux-gnu/xsecurelock/dimmer"
+DIMMER="/usr/libexec/xsecurelock/dimmer"
 
 xset s ${LOCK_TIME} ${CYCLE_TIME}
+xset dpms ${LOCK_TIME} ${LOCK_TIME} ${LOCK_TIME} 
 
 # X11 blank after lock.
 export XSECURELOCK_BLANK_TIMEOUT=5
