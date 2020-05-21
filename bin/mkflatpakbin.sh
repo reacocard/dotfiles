45c938b60c891491d1ac5dcee197ef5573490c39
Ayla Ounce <reacocard@gmail.com>
2020-05-20 20:08:49 -0700

    add script to turn flatpak .desktop into executables

--- bin/mkflatpakbin.sh
+++ bin/mkflatpakbin.sh
@@ -6,6 +6,8 @@
 set -o nounset
 set -o pipefail
 
+DEBUG=1
+
 if [ -z "$1" ]; then
 	echo 'Must specify target dir as first argument.' >&2
 	exit 1
@@ -17,20 +19,26 @@ if [ -z "$XDG_DATA_DIRS" ]; then
 fi
 
 for data_dir in ${XDG_DATA_DIRS//:/ }; do
-	if [ ! -d $data_dir ] || [ ! -d $data_dir/applications ]; then
+	if [ ! -d "$data_dir" ] || [ ! -d "$data_dir/applications" ]; then
 		continue
 	fi
+	if [ $DEBUG ]; then echo -e "=== data dir: $data_dir ===\n"; fi
 	for desktop in ${data_dir}applications/*.desktop; do
 		execline=`< $desktop grep --max-count=1 '^Exec='`
 		if echo "$execline" | grep -v -q 'bin/flatpak'; then
 			continue
 		fi
+		if [ $DEBUG ]; then echo "execline: $execline"; fi
 
-		command_name=`echo $execline | egrep --only-matching '[A-Za-z0-9]+(\.[A-Za-z0-9]+)+'`
+		command_name=`echo $execline | egrep --only-matching '\w[A-Za-z0-9_]+(\.[A-Za-z0-9_]+)+\w'`
+		if [ $DEBUG ]; then echo "command name: $command_name"; fi
 		command_exec=`echo $execline | cut -c 6- | sed -E 's/ (@@|%)[^ ]*//g'`
+		if [ $DEBUG ]; then echo "command exec: $command_exec"; fi
 		target_filename="$1/$command_name"
-		echo -e "#!/bin/sh\nexec $command_exec \"\$@\"" > $target_filename
-		chmod +x $target_filename
+		if [ $DEBUG ]; then echo "target filename: $target_filename"; fi
+		echo -e "#!/bin/sh\nexec $command_exec \"\$@\"" > "$target_filename"
+		chmod +x "$target_filename"
+		if [ $DEBUG ]; then echo; fi
 	done
 done
 exit 0
