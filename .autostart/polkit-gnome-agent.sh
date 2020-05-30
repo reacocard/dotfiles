3decc607acad7ced85459d360b1df04199bdae10
Ayla Ounce <reacocard@gmail.com>
2020-05-30 14:03:03 -0700

    [autostart] fix polkit agent script

--- .autostart/polkit-gnome-agent.sh
+++ .autostart/polkit-gnome-agent.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-PATHS='/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1'
+PATHS=(/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 /usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1)
 
 for path in $PATHS; do
     if [ -x "$path" ]; then
