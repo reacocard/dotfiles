91260509dc226e32dacca89055ad0990717545eb
Ayla Ounce <reacocard@gmail.com>
2020-07-15 19:06:00 -0700

    update toggle_touchpad.sh for gnome

--- bin/toggle_touchpad.sh
+++ bin/toggle_touchpad.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-synclient TouchpadOff=$(expr 1 - $(synclient -l | grep TouchpadOff | cut -b 31))
+if [ x"$XDG_CURRENT_DESKTOP" = x"GNOME" ]; then
+    DCONF_KEY='/org/gnome/desktop/peripherals/touchpad/send-events'
+    current=`dconf read $DCONF_KEY`
+    if [ x"$current" = x"'enabled'" ]; then
+        dconf write $DCONF_KEY "'disabled'"
+    else
+        dconf write $DCONF_KEY "'enabled'"
+    fi
+else
+    synclient TouchpadOff=$(expr 1 - $(synclient -l | grep TouchpadOff | cut -b 31))
+fi
